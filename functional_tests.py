3f6b31a26e379a507c96d9f7c02785a98c4eaebb
why724809024 <hbhs6855@163.com>
2020-04-07 11:07:30 +0800

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 import unittest
+from selenium.webdriver.common.keys import Keys
+import time
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser=webdriver.Firefox()
@@ -11,18 +13,36 @@ class NewVisitorTest(unittest.TestCase):
         # 伊迪丝听说了一个很酷的新的看板系统。她去看它的主页。
         self.browser.get('http://localhost:8000') #打开django项目页面
 
-        #注意到标题显示待办事项列表，她立即进入待办事项
+        # 注意到标题显示待办事项列表，她立即进入待办事项
         self.assertIn('To-Do',self.browser.title) #断言title为‘To-Do’,并输出title
-        self.fail('Finish the test!') #测试结束~
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        #She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        #她在文本框中输入“购买孔雀羽毛”（伊迪丝的爱好是做飞鱼标本）
+        inputbox.send_keys('Buy peacock feathers')
+
+        #当她点击进入时，页面会更新现在页面会列出  
+        # “1：购买孔雀羽毛”，作为待办事项列表中的项目
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
         
-
-#她在文本框中输入“购买孔雀羽毛”（伊迪丝的爱好是做飞鱼标本）
-
-#当她点击进入时，页面会更新现在页面会列出  
-# “1：购买孔雀羽毛”，作为待办事项列表中的项目
-
-#接着一个文本框邀请她添加另一个项目。
-#她输入“使用孔雀羽毛制作苍蝇标本”（伊迪丝非常有条不紊）
+        #接着一个文本框邀请她添加另一个项目。
+        #她输入“使用孔雀羽毛制作苍蝇标本”（伊迪丝非常有条不紊）
+        self.fail('Finish the test!') #测试结束~
 
 #页面再次更新，现在在她的列表中显示这两个项目
 
