263c0e85ddcd86548d8a8871d55d401ad6e5fdea
why724809024 <hbhs6855@163.com>
2020-04-21 11:18:00 +0800

    a better f_tests

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 伊迪丝听说了一个很酷的新的看板系统。她去看它的主页。
         self.browser.get('http://localhost:8000')  # 打开django项目页面
@@ -34,6 +39,7 @@ class NewVisitorTest(unittest.TestCase):
         # “1：购买孔雀羽毛”，作为待办事项列表中的项目
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # 接着一个文本框邀请她添加另一个项目。
         # 她输入“使用孔雀羽毛制作苍蝇标本”（伊迪丝非常有条不紊）
@@ -43,10 +49,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # 页面再次更新，现在在她的列表中显示这两个项目
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # 伊迪丝想知道该网站是否会记住她的列表。
         # 然后，她看到该网站为她生成了一个唯一的URL，且有一些解释性的文本。
